0a470e7a0b8ad4cd1188978baf7fff03630c6a37
kaifronsdal <kaifronsdal@gmail.com>
2023-05-20 14:19:55 -0700

    Now able to run arbitrary make commands

--- upycoq-src/k-pycoq/opam.py
+++ upycoq-src/k-pycoq/opam.py
@@ -1,12 +1,12 @@
-import re
+"""Module for running commands in opam switch."""
+
 import subprocess
 from pathlib import Path
-from typing import Union, List
 from subprocess import CompletedProcess
+import shlex
 
 
-def run(command: Union[str, List[str]], switch: str, cwd: Path, **kwargs) \
-        -> Union[List[CompletedProcess], CompletedProcess]:
+def run(command: str, switch: str, cwd: Path, **kwargs) -> CompletedProcess:
     """Runs command with opam switch.
 
     :param command: command to run
@@ -15,25 +15,74 @@ def run(command: Union[str, List[str]], switch: str, cwd: Path, **kwargs) \
     :param kwargs: kwargs passed to subprocess
     :return: list of subprocess.CompletedProcess objects
     """
-
     check_switch_installed(switch, cwd, **kwargs)
 
-    results = []
+    opam_command: str = f'opam exec --switch {switch} -- sh -c {shlex.quote(command)}'
+    result = subprocess.run(opam_command, capture_output=True, shell=True, cwd=cwd, **kwargs)
 
-    # This could break with more complex commands
-    # split commands by && and ; and run them in order
-    # TODO: find way to run general commands in opam switch
-    for c in re.split(r'&&|;', command):
-        opam_command: str = f'opam exec --switch {switch} -- {c}'
-        result = subprocess.run(opam_command.split(), capture_output=True, cwd=cwd, **kwargs)
-        results.append(result)
-
-    return results if len(results) > 1 else results[0]
+    return result
 
 
 def check_switch_installed(switch: str, cwd: Path, **kwargs):
     """Checks if opam switch is found. Raises RuntimeError if switch was not found."""
-    result = subprocess.run(f'opam exec --switch {switch} -- true', capture_output=True, cwd=cwd, **kwargs)
+    result = subprocess.run(f'opam exec --switch {switch} -- true'.split(), capture_output=True, cwd=cwd, **kwargs)
     if result.returncode != 0:
-        raise RuntimeError(f'Failed to find opam switch {switch}.\n'
-                           f'You can create a new opam switch with `opam switch create {switch}`.\n')
+        raise RuntimeError(f'Failed to set opam switch "{switch}".\n'
+                           f'{result.stderr.decode()}')
+
+
+if __name__ == '__main__':
+    switch = 'ocaml-variants.4.07.1+flambda_coq-serapi.8.11.0+0.11.1'
+
+    # test basic functionality
+    result = run('not a command', switch, Path.cwd())
+    assert result.stdout.decode() == '' and result.stderr.decode() == "sh: not: command not found\n"\
+           and result.returncode == 127
+
+    result = run('echo "hello world"', switch, Path.cwd())
+    assert result.returncode == 0 and result.stdout.decode() == 'hello world\n'
+
+    result = run('This should error; echo "hello world"', switch, Path.cwd())
+    assert result.stderr.decode() == "sh: This: command not found\n" \
+           and result.stdout.decode() == 'hello world\n' \
+           and result.returncode == 0
+
+    result = run('This should error && echo "hello world"', switch, Path.cwd())
+    assert result.stderr.decode() == "sh: This: command not found\n" \
+           and result.stdout.decode() == '' \
+           and result.returncode == 127
+
+    result = run('echo "hello world" ; echo "bye world"', switch, Path.cwd())
+    assert result.returncode == 0 and result.stdout.decode() == 'hello world\nbye world\n'
+
+    result = run('echo "hello world" || echo "bye world"', switch, Path.cwd())
+    assert result.returncode == 0 and result.stdout.decode() == 'hello world\n'
+
+    result = run('echo "hello world" ; echo "hello world" ; echo "hello world"', switch, Path.cwd())
+    assert result.returncode == 0 and result.stdout.decode() == 'hello world\nhello world\nhello world\n'
+
+    result = run('echo "hello world" ; echo "hello world" && echo "hello world" ; echo "bye world"', switch,
+                 Path.cwd())
+    assert result.returncode == 0 \
+           and result.stdout.decode() == 'hello world\nhello world\nhello world\nbye world\n'
+
+
+    # test switch not found
+    try:
+        run('echo "hello world"', 'not a switch', Path.cwd())
+        assert False  # should not reach here
+    except RuntimeError as e:
+        assert str(e) == 'Failed to set opam switch "not a switch".\n' \
+                         '[ERROR] The selected switch not is not installed.\n'
+
+    # test cwd
+    result = run('pwd', switch, Path.cwd())
+    assert result.returncode == 0 and result.stdout.decode() == f'{Path.cwd()}\n'
+
+    # test kwargs
+    import os
+    env = {**os.environ, 'USER': 'test'}
+    result = run('echo $USER', switch, Path.cwd(), env=env)
+    assert result.returncode == 0 and result.stdout.decode() == 'test\n'
+
+    print('All tests passed!')
