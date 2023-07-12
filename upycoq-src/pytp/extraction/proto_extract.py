600459c12b99493a8f7795a8b24414e74052f55e
jizej <jiangjize2000@gmail.com>
2023-07-11 20:36:57 -0700

    parse files for checkpoints and aug text

--- upycoq-src/pytp/extraction/proto_extract.py
+++ upycoq-src/pytp/extraction/proto_extract.py
@@ -39,9 +39,77 @@ from pytp.coq.converter import get_coq_lsp_converter
 
 from pytp.coq.coq_lsp_client import CoqLSPClient, get_default_coq_lsp_config
 
+from itertools import tee, zip_longest
+
 """
 TODO: host a working client outside ./coq, do we have enough utility functions provided by coq_lsp_client ?
 """
+
+
+"""
+Some utility function to be moved elsewhere later
+"""
+def split_by_idx(S, list_of_indices):
+    left, right = 0, list_of_indices[0]
+    yield S[left:right]
+    left = right
+    for right in list_of_indices[1:]:
+        yield S[left:right]
+        left = right
+    yield S[left:]
+
+
+def find_all(s, c):
+    """
+    Given a charactor, find index of each each appearance.
+    """
+    idx = s.find(c)
+    while idx != -1:
+        yield idx
+        idx = s.find(c, idx + 1)
+
+
+"""
+Extraction
+"""
+def parse_proof_file(file_path: Path):
+    """
+    Given a proof file, parse information that are needed later in extraction:
+    - text: the raw text of the proof file
+    - augmented_text: text with appended 'show proof' queries.
+    - num_lines: number of lines
+    - checkpoints: positions of each '.' (i.e. end of each statement)
+    - augmented_checkpoints = [(position of original statement, position of 'show proof' query), ...]
+    """
+    text = ""
+    augmented_text = ""
+    statements = []
+    num_line = 0
+    checkpoints = []
+    augmented_checkpoints = []
+    with open(file_path, 'r') as file:
+        for idx, each in enumerate(file):
+            text += each
+            num_line += 1
+
+            # determine checkpoints in the original text
+            checkpoint = list(find_all(each, '.'))
+            checkpoints.append(zip([idx * len(checkpoint)], checkpoint))
+            print(checkpoint)
+            
+            # split text by '.' to get individual statements
+            if len(checkpoint):
+                statements_in_line = list(split_by_idx(each, checkpoint))
+                statements.append(statements_in_line)
+                print(statements_in_line)
+            else:
+                statements.append([])
+                print('no statement')
+            
+            # create augmented text and checkpoints
+
+    return text, num_line, checkpoints, statements
+
 
 def winston_coq_lsp():
     # test coq-lsp
@@ -76,33 +144,55 @@ def winston_coq_lsp():
     ))
 
     # open file
+    proof_path = new_workspace_path / 'DebugSimpleArith.v'
+    proof_text, proof_num_lines, proof_checkpoints, proof_statements = parse_proof_file(proof_path)
+    print("===========")
+    print(proof_text)
+    print("===========")
     client.text_document_did_open(params=lsp_types.DidOpenTextDocumentParams(
         text_document=lsp_types.TextDocumentItem(
-            uri=(new_workspace_path / 'DebugSimpleArith.v').as_uri(),
+            uri=(proof_path).as_uri(),
             language_id='coq',
             version=1,
-            text='Require Import Arith.\n'
-                 'Theorem plus_0_n : forall n : nat, 0 + n = n.\n'
-                 'Proof.\n'
-                 '  intros n.\n'
-                 '  simpl.\n'
-                 '  reflexivity.\n'
-                 'Qed.'
+            text = proof_text 
         )
     ))
 
-    # get goals
-    id = client.proof_goals(params=GoalRequest(text_document=lsp_types.VersionedTextDocumentIdentifier(
-        uri=(new_workspace_path / 'DebugSimpleArith.v').as_uri(),
-        version=1
-    ), position=lsp_types.Position(line=4, character=4)))
+    # get goals for each line
+    proof_goals = []
+    proof_messages = []
+    for i in range(proof_num_lines):
+        # get proof goals from coq
+        id = client.proof_goals(params=GoalRequest(text_document=lsp_types.VersionedTextDocumentIdentifier(
+            uri=(new_workspace_path / 'DebugSimpleArith.v').as_uri(),
+            version=1
+        ), position=lsp_types.Position(line=i, character=0)))
 
-    print(f'Goals: {client.wait_for_response(id)}')
+        # get proof terms for each statement
+
+        response = client.wait_for_response(id)
+        print(f'Goals at line {i}: {response}')
+
+        proof_messages.append(response.messages)
+        proof_goals.append(response.goals)
+
+        # handle proof related meta-data
+        pass
+    
+    print("===========")
+    for idx, each in enumerate(proof_goals):
+        print(idx, each)
+    print("===========")
+    print("===========")
+    for idx, each in enumerate(proof_messages):
+        print(idx, each)
+    print("===========")
 
     # close client
     client.close()
+
     print('Tests passed!')
 
 
 if __name__ == '__main__':
-    example_coq_lsp()
+    winston_coq_lsp()
