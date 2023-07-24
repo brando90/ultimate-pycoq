58395913774c5e7408000330c4e51c901964f7d9
L-Penguin <jiangjize2000@gmail.com>
2023-07-24 00:06:30 -0700

    format proofs to into steps; remove bullet level

--- upycoq-src/pytp/extraction/proto_extract.py
+++ upycoq-src/pytp/extraction/proto_extract.py
@@ -84,7 +84,7 @@ def find_all(s, c):
 """
 Extraction
 """
-def parse_proof_file(file_path: Path):
+def old_parse_proof_file(file_path: Path) :
     """
     Given a proof file, parse information that are needed later in extraction:
     - text: the raw text of the proof file
@@ -175,7 +175,6 @@ def parse_proof_file(file_path: Path):
                 qed_find = each.find('Qed')
 
 
-
             # determine checkpoints in the original text
             checkpoint = list(find_all(each, '.'))
             checkpoints.append(zip([idx * len(checkpoint)], checkpoint))
@@ -199,6 +198,197 @@ def parse_proof_file(file_path: Path):
     return text, num_line, checkpoints, statements
 
 
+def get_text(file_path: Path):
+    with open(file_path, 'r') as file:
+        text = ''
+        lines = []
+        for each in file:
+            text += each
+            lines.append(each)
+        return text, lines
+
+
+def get_theorem_symbols(document_symbol):
+    theorems = []
+    for each_symbol in document_symbol:
+        if each_symbol.detail == 'Theorem':
+            theorems.append(each_symbol)
+    return theorems
+
+
+def get_theorem_name(theorem_symbol):
+    return theorem_symbol.name
+
+
+def get_theorem_text(theorem_symbol, text_lines, return_range = False):
+    """Get the text of a theorem based on its symbol
+
+    Note that the range given by the symbol only goes so far to its the end of definition, not the end of proof.
+
+    Args:
+        theorem_symbol (symbol): _description_
+        text_lines (list of str): _description_
+
+    Returns:
+        str: the full text of the theorem
+    """
+    assert theorem_symbol.detail == 'Theorem'
+    start_line = theorem_symbol.range.start.line
+    end_line = theorem_symbol.range.end.line
+    start_char = theorem_symbol.range.start.character
+    end_char = theorem_symbol.range.end.character
+    thm_text = ''
+    
+    # gather within given range
+    for i in range(start_line, end_line + 1):
+        if i == start_line:
+            thm_text += text_lines[i][start_char:]
+        elif i == end_line:
+            thm_text += text_lines[i][:end_char+1]
+        else:
+            thm_text += text_lines[i]
+   
+    # find the proof
+    proof_text = ''
+    proof_start_line = end_line
+    proof_start_char = end_char + 1
+    if end_char + 1 == len(text_lines[end_line]) : # theorem def reaches end of line
+        proof_start_line = end_line + 1
+        proof_start_char = 0
+
+    proof_end_line = proof_start_line
+    proof_end_char = 0
+    # first see if a proof exists, that is if theorems ends with 'Qed.'
+    for i in range(proof_start_line, len(text_lines)):
+        start_char = 0
+        if i == proof_start_line:
+            start_char = proof_start_char
+        proof_text += text_lines[i][start_char:]
+        if 'Qed.' in text_lines[i][start_char:]:
+            proof_end_line = i
+            proof_end_char = text_lines[i][start_char:].find('Qed.') + 4
+            break
+        if 'Abort.' in text_lines[i][start_char:]:
+            proof_end_line = i
+            proof_end_char = text_lines[i][start_char:].find('Abort.') + 6
+            break
+        
+    if return_range:
+        return thm_text + proof_text, [start_line, start_char, end_line, end_char], [proof_start_line, proof_start_char, proof_end_line, proof_end_char]
+    else:
+        return thm_text + proof_text
+
+
+def get_theorem_definition(theorem_text):
+    """Get the definition of a theorem from its text
+
+    Returns:
+        definition_text (str): the text of the theorem definition
+    """
+    definition_start = theorem_text.find(':')
+    definition_end = theorem_text.find('Proof.')
+    
+    if definition_end == -1: # if the 'Proof.' keyword is not used
+        definition_end = theorem_text.find('.') + 1
+    
+    definition_text = theorem_text[definition_start + 1: definition_end]
+        
+    return definition_text
+
+
+def get_theorem_proof(theorem_text):
+    """Get the proof of a theorem from its text
+
+    Returns:
+        proof_text (str): the text of the theorem proof
+    """
+    proof_start = theorem_text.find('Proof.')
+    proof_end = theorem_text.find('Qed.')
+
+    proof_text = ''
+
+    if proof_start == -1: # if the 'Proof.' keyword is not used
+        proof_start = theorem_text.find('.') + 1
+    else:
+        proof_start += 6
+    
+    proof_text = theorem_text[proof_start: proof_end]
+        
+    return proof_text
+
+
+def format_proof_text(proof_text):
+    """Format the proof text to remove bullet points, comments and indentation
+    
+    TODO: support bullet level
+
+    Args:
+        proof_text (_type_): _description_
+    """
+
+    proof_texts = [each.strip() for each in proof_text.split('\n')]
+
+    for each_idx, each in enumerate(proof_texts):
+        # remove bullet points
+        re_front_bullet = re.compile(r'^[\*\-{\+]+')
+        proof_texts[each_idx] = re_front_bullet.sub('', each).replace('}','').strip()
+
+        # remove comments
+        re_comment = re.compile(r'\(\*.*\*\)')
+        proof_texts[each_idx] = re_comment.sub('', proof_texts[each_idx]).strip()
+    
+    idx = 0 
+    while idx < len(proof_texts): 
+        # break lines at each step
+        line_split = proof_texts[idx].split('.')
+        if len(line_split) > 2:
+            proof_texts[idx] = line_split[0] + '.'
+            for j, each in enumerate(line_split[1:-1]):
+                proof_texts.insert(idx + j + 1, each.strip() + '.')
+            idx += len(line_split) - 1
+        else:
+            idx += 1
+    
+    proof_texts = filter(lambda x: x != '', proof_texts)
+    
+    return '\n'.join(proof_texts)
+
+
+def get_augmented_proof_steps(formated_proof_text):
+    proof_steps = formated_proof_text.split('\n')
+    augmented_proof_steps = [proof_steps + 'Show Proof.' for proof_steps in proof_steps]
+    return augmented_proof_steps
+
+
+def mark_checkpoints_in_range(text_lines, start_line, start_char, end_line, end_char):
+    check_points = []
+    for l, each in enumerate(text_lines[start_line:end_line+1]):
+        l += start_line
+        check_points += [*[[l, i] for i in find_all(each, '.') if i != len(each) - 1 and (l != 0 or i != 0) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char) and (l != end_line or i != end_char) and (l != start_line or i != start_char)]]
+    return check_points
+
+
+def substitute_augmented_proof_steps(text_lines, augmented_proof_steps, proof_start_line, proof_start_char, proof_end_line, proof_end_char):
+    for i in range(len(augmented_proof_steps)):
+        if i == 0:
+            text_lines[proof_start_line] = text_lines[proof_start_line][:proof_start_char] + augmented_proof_steps[0]
+        elif i == len(augmented_proof_steps):
+            text_lines[proof_start_line] += augmented_proof_steps[-1] + text_lines[proof_start_line][proof_end_char:]
+        else:
+            text_lines[proof_start_line] += augmented_proof_steps[i]
+    for i in range(proof_start_line+1, proof_end_line+1):
+        if i == proof_end_line and proof_end_char != len(text_lines[i]) - 1:
+            text_lines[i] = text_lines[i][proof_end_char+1:]
+        else:
+            text_lines[i] = '\n'
+    return text_lines
+
+
+def parse_proof_file(file_path: Path):
+    text, lines = get_text(file_path)
+    return text, len(lines), lines
+
+
 def winston_coq_lsp():
     # test coq-lsp
     config = get_default_coq_lsp_config()
@@ -220,6 +410,7 @@ def winston_coq_lsp():
     print('Initialized')
 
     new_workspace_path = Path('~/ultimate-pycoq/coq-projects/debug/debug_simple_arith').expanduser()
+    new_workspace_path = Path('~/ultimate-pycoq/coq-projects/basic-lf/lf').expanduser()
 
     # change workspace folder
     client.workspace_did_change_workspace_folders(params=lsp_types.DidChangeWorkspaceFoldersParams(
@@ -232,17 +423,18 @@ def winston_coq_lsp():
     ))
 
     # open file
-    proof_path = new_workspace_path / 'DebugSimpleArith.v'
-    proof_text, proof_num_lines, proof_checkpoints, proof_statements = parse_proof_file(proof_path)
+    proof_path = new_workspace_path / 'debug_0_plus_n_eq_n.v'
+    proof_path = new_workspace_path / 'Basics.v'
+    file_text, num_lines, text_lines = parse_proof_file(proof_path)
     print("===========")
-    print(proof_text)
+    print(file_text)
     print("===========")
     client.text_document_did_open(params=lsp_types.DidOpenTextDocumentParams(
         text_document=lsp_types.TextDocumentItem(
             uri=(proof_path).as_uri(),
             language_id='coq',
             version=1,
-            text = proof_text 
+            text = file_text 
         )
     ))
 
@@ -251,43 +443,64 @@ def winston_coq_lsp():
             uri=(proof_path).as_uri(),
             language_id='coq',
             version=1,
-            text = proof_text 
+            text = file_text 
         )
     ))
-    response = client.wait_for_response(id)
-    print(f'Document Symbols: {response}')
+    document_symbols = client.wait_for_response(id)
+    print(f'Document Symbols: {document_symbols}')
 
-    # get goals for each line
-    proof_goals = []
-    proof_messages = []
-    for i in range(proof_num_lines):
-        # get proof goals for each statement
-        # line_goals = []
-        # for ckpt in proof_checkpoints[i]:
-        #     id = client.proof_goals(params=GoalRequest(text_document=lsp_types.VersionedTextDocumentIdentifier(
-        #         uri=(new_workspace_path / 'DebugSimpleArith.v').as_uri(),
-        #         version=1
-        #     ), position=lsp_types.Position(line=i, character=0)))
-
-        # get proof terms for each statement
-
-        response = client.wait_for_response(id)
-        print(f'Goals at line {i}: {response}')
-
-        proof_messages.append(response.messages)
-        proof_goals.append(response.goals)
-
-        # handle proof related meta-data
-        pass
+    # get theorems
+    thm_symbols = get_theorem_symbols(document_symbols)
+    theorems = {}
+    for each_thm_symbol in thm_symbols:
+        thm_text, def_ranges, proof_ranges = get_theorem_text(each_thm_symbol, text_lines, return_range=True)
+        thm_def = get_theorem_definition(thm_text)
+        thm_proof = get_theorem_proof(thm_text)
+        theorems[get_theorem_name(each_thm_symbol)] = {
+            'text': thm_text,
+            'definition': thm_def, 
+            'proof': thm_proof,
+            'def_ranges': def_ranges, # [start_line, start_char, end_line, end_char]
+            'proof_ranges': proof_ranges,
+        }
     
-    print("===========")
-    for idx, each in enumerate(proof_goals):
-        print(idx, each)
-    print("===========")
-    print("===========")
-    for idx, each in enumerate(proof_messages):
-        print(idx, each)
-    print("===========")
+    for k, v in theorems.items():
+        print("=======================")
+        print(f'Theorem: {k}') 
+        print(f'Definition Range: {v["def_ranges"]}')
+        print(f'Proof Range: {v["proof_ranges"]}')
+        print("===========")
+        print(f'Text: {v["text"]}')
+        print("===========")
+        print(f'Definition: {v["definition"]}')
+        print("===========")
+        print(f'Proof: {v["proof"]}')
+        print("===========")
+        print(f'Formated Proof: {format_proof_text(v["proof"])}')
+        print("===========")
+        text_lines = substitute_augmented_proof_steps(text_lines, get_augmented_proof_steps(format_proof_text(v["proof"])), *v['proof_ranges'])
+        new_thm_text = "".join(text_lines[v["def_ranges"][0]:v["proof_ranges"][2]])
+        print(f'Augmented Text: {new_thm_text}')
+        print("===========")
+        checkpoints = mark_checkpoints_in_range(text_lines, v['def_ranges'][0],v['def_ranges'][1],v['proof_ranges'][2],v['proof_ranges'][3])
+        print(f'Checkpoints: {checkpoints}')
+        print("===========")
+        id = client.text_document_did_change(params=lsp_types.DidChangeTextDocumentParams(
+            text_document=lsp_types.VersionedTextDocumentIdentifier(
+                uri=(proof_path).as_uri(),
+                version=1
+            ),
+            content_changes=[lsp_types.TextDocumentContentChangeEvent(text=''.join(text_lines))]
+        ))
+        goal = client.wait_for_response(id)
+        for each in checkpoints:
+            id = client.proof_goals(params=GoalRequest(text_document=lsp_types.VersionedTextDocumentIdentifier(
+                                    uri=(proof_path).as_uri(),
+                                    version=1
+                                    ), position=lsp_types.Position(line=each[0], character=each[1])))
+            goal = client.wait_for_response(id)
+            print(f'Goal at {each}: {goal}')
+        print("=======================")
 
     # close client
     client.close()
